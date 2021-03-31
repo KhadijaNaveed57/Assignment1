9aa73c6eb70dcee7f5e9f74d29857fece0041dc2
KhadijaNaveed57 <mcaf19a0312pucit.edu.pk>
2021-03-31 23:37:07 +0500

    Second Change

---
+++ Final/app/src/main/java/com/khadijanaveed/assignment_project/MainActivity.java
@@ -0,0 +1,22 @@
+package com.khadijanaveed.assignment_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView = findViewById(R.id.bismillah);
+        imageView.animate().alpha(0).setDuration(9000);
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.bismillah);
+        mp.start();
+
+
+    }
+}
