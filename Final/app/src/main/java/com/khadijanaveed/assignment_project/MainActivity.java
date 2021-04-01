fa9dd0d946b03de37bb72d9429c531537aa489b0
KhadijaNaveed57 <mcaf19a0312pucit.edu.pk>
2021-04-01 17:43:37 +0500

    First Page almost done

--- Final/app/src/main/java/com/khadijanaveed/assignment_project/MainActivity.java
+++ Final/app/src/main/java/com/khadijanaveed/assignment_project/MainActivity.java
@@ -2,12 +2,17 @@ package com.khadijanaveed.assignment_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
+    Timer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +21,13 @@ public class MainActivity extends AppCompatActivity {
         imageView.animate().alpha(0).setDuration(9000);
         MediaPlayer mp = MediaPlayer.create(this,R.raw.bismillah);
         mp.start();
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,secondPage.class);
+                startActivity(intent);
+            }
+        },9000);
     }
 }
